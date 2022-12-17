8cfb87425c53945a35e068e32d4e41a3b06b6319
Martti Yap <marttiyap@gmail.com>
2022-12-17 22:38:26 +0100

    add half of day 8

--- 2022/go/day08/main.go
+++ 2022/go/day08/main.go
@@ -7,42 +7,117 @@ import (
 	"strings"
 )
 
+func find_vis_left_to_right(trees [][]int, dy int, dx int, vis [][]int) [][]int {
+
+	for i := 0; i < dy; i++ {
+		row_max := 0
+		for j := 0; j < dx; j++ {
+			if trees[i][j] > row_max {
+				vis[i][j] = 1
+				row_max = trees[i][j]
+			}
+		}
+	}
+	return vis
+}
+func find_vis_up_to_down(trees [][]int, dy int, dx int, vis [][]int) [][]int {
+	// this is really just left to right with transposed grids
+	for i := 0; i < dy; i++ {
+		row_max := 0
+		// fmt.Println("row reset")
+		for j := 0; j < dx; j++ {
+			// fmt.Println(trees[j][i])
+			// trees and vis transposed
+			if trees[j][i] > row_max {
+				vis[j][i] = 1
+
+				// fmt.Println("vis=1", "at", j, i)
+				row_max = trees[j][i]
+				// fmt.Println("new max:", row_max)
+			}
+		}
+	}
+	return vis
+}
+
+func find_vis_down_to_up(trees [][]int, dy int, dx int, vis [][]int) [][]int {
+	// this is really just right to left with transposed grids
+
+	for i := 0; i < dy; i++ {
+		row_max := 0
+		for j := dx - 1; j >= 0; j-- {
+			if trees[j][i] > row_max {
+				vis[j][i] = 1
+				row_max = trees[j][i]
+			}
+		}
+	}
+	return vis
+}
+func find_vis_right_to_left(trees [][]int, dy int, dx int, vis [][]int) [][]int {
+	for i := 0; i < dy; i++ {
+		row_max := 0
+		for j := dx - 1; j >= 0; j-- {
+			if trees[i][j] > row_max {
+				vis[i][j] = 1
+				row_max = trees[i][j]
+			}
+		}
+	}
+	return vis
+}
+
 func main() {
 
-	input, _ := ioutil.ReadFile("example.txt")
-	// input, _ := ioutil.ReadFile("input.txt")
+	// input, _ := ioutil.ReadFile("example.txt")
+	input, _ := ioutil.ReadFile("input.txt")
 	splitInput := strings.Split(strings.TrimSpace(string(input)), "\n")
 
 	// use (y,x) convention. Y is down. and first in tuple
-	var x_len, y_len int
-	x_len = len(strings.TrimSpace(splitInput[0]))
-	y_len = len(splitInput)
+	var dx, dy int
+	dx = len(strings.TrimSpace(splitInput[0]))
+	dy = len(splitInput)
 
-	tree_grid := make([][]int, y_len)
-	vis := make([][]int, y_len)
+	tree_grid := make([][]int, dy)
+	vis := make([][]int, dy)
+	final_vis := make([][]int, dy)
 
-	fmt.Println("gridsize:", y_len, x_len)
+	fmt.Println("gridsize:", dy, dx)
 
-	for i := 0; i < y_len; i++ {
-		tree_grid[i] = make([]int, x_len)
-		vis[i] = make([]int, x_len)
+	for i := 0; i < dy; i++ {
+		tree_grid[i] = make([]int, dx)
+		vis[i] = make([]int, dx)
+		final_vis[i] = make([]int, dx)
 
 		row := splitInput[i]
-		for j := 0; j < x_len; j++ {
+		for j := 0; j < dx; j++ {
 			height, _ := strconv.Atoi(string(row[j]))
 			tree_grid[i][j] = height
 
-			if i == 0 || i == y_len-1 {
+			if i == 0 || i == dy-1 {
 				vis[i][j] = 1
 
 			}
-			if j == 0 || j == x_len-1 {
+			if j == 0 || j == dx-1 {
 				vis[i][j] = 1
 
 			}
 		}
 	}
 
-	fmt.Println(tree_grid)
-	fmt.Println(vis)
+	vis1 := find_vis_left_to_right(tree_grid, dy, dx, vis)
+	vis2 := find_vis_right_to_left(tree_grid, dy, dx, vis)
+	vis3 := find_vis_up_to_down(tree_grid, dy, dx, vis)
+	vis4 := find_vis_down_to_up(tree_grid, dy, dx, vis)
+
+	counter := 0
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dx; j++ {
+			if vis1[i][j] == 1 || vis2[i][j] == 1 || vis3[i][j] == 1 || vis4[i][j] == 1 {
+				counter += 1
+			}
+		}
+	}
+
+	fmt.Println("ans:", counter)
 }
