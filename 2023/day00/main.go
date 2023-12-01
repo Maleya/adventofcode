f17a29bbb287625f797a90831d607490f52d5a2f
Martti Yap <martti@ip-10-1-10-48.eu-west-2.compute.internal>
2023-12-01 13:18:13 +0100

    clean up template file

---
+++ 2023/day00/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// fileName := "example.txt"
+	fileName := "input.txt"
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Read the file content
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	splitInput := strings.Split(strings.TrimSpace(string(content)), "\n")
+
+	for _, line := range splitInput {
+		fmt.Println(line)
+	}
+}
