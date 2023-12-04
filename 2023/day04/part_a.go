920d80a574e338c023322b3945ce2005daf7c4d6
Martti Yap <martti.yap@gmail.com>
2023-12-04 08:19:05 +0100

    parsing complete

--- 2023/day04/part_a.go
+++ 2023/day04/part_a.go
@@ -4,12 +4,43 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
+func parseInput(input string) (int, []int, []int, error) {
+	var card_no int
+
+	parts := strings.Split(input, "|")
+	left_split := strings.Split(parts[0], ":")
+
+	fmt.Sscanf(input, "Card %d", &card_no)
+
+	num_left := strings.TrimSpace(left_split[1])
+	num_right := strings.TrimSpace(parts[1])
+	numbers1, _ := parseNumbers(strings.TrimSpace(num_left))
+	numbers2, _ := parseNumbers(strings.TrimSpace(num_right))
+	return card_no, numbers1, numbers2, nil
+}
+
+func parseNumbers(numbersStr string) ([]int, error) {
+	numberStrings := strings.Fields(numbersStr)
+	numbers := make([]int, len(numberStrings))
+
+	for i, numStr := range numberStrings {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse number: %v", err)
+		}
+		numbers[i] = num
+	}
+
+	return numbers, nil
+}
+
 func main() {
-	// fileName := "example.txt"
-	fileName := "input.txt"
+	fileName := "example.txt"
+	// fileName := "input.txt"
 
 	file, _ := os.Open(fileName)
 	defer file.Close()
@@ -17,8 +48,12 @@ func main() {
 
 	splitInput := strings.Split(strings.TrimSpace(string(content)), "\n")
 	for _, line := range splitInput {
-
 		fmt.Println(line)
+
+		cardName, numbers1, numbers2, _ := parseInput(line)
+		fmt.Println("Card Name:", cardName)
+		fmt.Println("Numbers Set 1:", numbers1)
+		fmt.Println("Numbers Set 2:", numbers2)
 
 	}
 }
