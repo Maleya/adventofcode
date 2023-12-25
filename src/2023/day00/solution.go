a87e85f66b42f474d3cdf3e666144ecda0dc9cea
Martti Yap <martti.yap@gmail.com>
2023-12-25 22:38:57 +0100

    refactor template

---
+++ src/2023/day00/solution.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+//go:embed example.txt
+var example_input string
+
+func partA(input []string) {
+	fmt.Println("part_a:")
+
+}
+func partb(input []string) {
+	fmt.Println("part_b:")
+}
+
+func main() {
+	// load_file := example_input
+	load_file := input
+	splitInput := strings.Split(strings.TrimSpace(string(load_file)), "\n")
+
+	for _, elem := range splitInput {
+		fmt.Println(elem)
+	}
+	partA(splitInput)
+	partb(splitInput)
+
+}
