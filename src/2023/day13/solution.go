98b82e1343cb2f4a75cde39eacc8006ba94b04d9
Martti Yap <martti.yap@gmail.com>
2024-01-05 13:38:07 +0100

    day13

--- src/2023/day13/solution.go
+++ src/2023/day13/solution.go
@@ -29,7 +29,6 @@ func (p Pattern) Print() {
 	}
 }
 
-// refactor to seperate lib?
 func (p Pattern) transpose() Pattern {
 	xl := len(p.Pattern[0])
 	yl := len(p.Pattern)
@@ -45,13 +44,15 @@ func (p Pattern) transpose() Pattern {
 	return Pattern{Pattern: result}
 }
 
-func (p Pattern) findvertical() int {
+// find the axis of reflection and returns [0] if none found (legacy)
+func (p Pattern) findvertical() []int {
 	// ax is the 0 indexed rightmost element of the mirror plane.
-	var potentialAxes []int
+	// therefore no natural reflections occur at 0
+	var potentialAxes, finalAxes []int
 	rows := len(p.Pattern)
 	cols := len(p.Pattern[0])
-	// fmt.Printf("rows: %d, cols: %d\n", rows, cols)
-	// find compare the first two columns:
+
+	// compare columns (pairwise)
 	for ax := 1; ax < len(p.Pattern[0]); ax++ {
 		var counter int
 		for _, line := range p.Pattern {
@@ -66,47 +67,97 @@ func (p Pattern) findvertical() int {
 		}
 	}
 	if len(potentialAxes) == 0 {
-		return 0
+		return []int{0}
 	}
-	// problems: The test case from reddit is giving 0,0
-	// and you dont catch if you dont find anything initially.
-	// its on the edge.. meaning the bottom part of the code cant be used and is not handled.
-	// Expand the search outwards
-	// fmt.Println("mirror axes to check:", potentialAxes)
 	for _, ax := range potentialAxes {
-		// fmt.Println("checking mirror axis", ax)
-
-		maxdist := min(ax-1, cols-ax-1) //! broken for ax=1
+		// expand the search outward
+		maxdist := min(ax-1, cols-ax-1)
 		if maxdist == 0 {
-			return ax
+			// catch case where no expansion needed:
+			finalAxes = append(finalAxes, ax)
+			continue
 		}
-		//* catch special case of no expansion needed.
 
 		var counter int
 		for i := 1; i <= maxdist; i++ {
-			// fmt.Println("expanding search to", ax-i-1, ax+i)
 			for j := 0; j < rows; j++ {
 				if p.Pattern[j][ax-i-1] == p.Pattern[j][ax+i] {
 					counter++
 					// fmt.Println("match on row", j, p.Pattern[j][ax-i-1], p.Pattern[j][ax+i])
 				}
 			}
-			// fmt.Println("counter", counter)
 			if counter == rows*maxdist {
-				// fmt.Println("found axis", ax)
-				return ax
-			} else {
-				// fmt.Println("axis", ax, "test incomplete", counter, "/", rows*maxdist)
+				finalAxes = append(finalAxes, ax)
+				continue
+			}
+		}
+	}
+	if len(finalAxes) == 0 {
+		return []int{0}
+	} else {
+		return finalAxes
+	}
+}
+func (p Pattern) findhorizontal() []int {
+	var axes []int
+	p = p.transpose()
+	axes = append(axes, p.findvertical()...)
+	return axes
+}
+
+// iterate all combinations of symbol swaps and record results
+func (p Pattern) iterateandtest() int {
+	pat := p.Pattern
+	var originalHort, originalVert int
+
+	originalHort = p.findhorizontal()[0]
+	originalVert = p.findvertical()[0]
+
+	for i := 0; i < len(pat); i++ {
+		for j := 0; j < len(pat[i]); j++ {
+
+			// make a copy of pattern slice
+			new_pat := make([][]string, len(pat))
+			for k := range pat {
+				new_pat[k] = make([]string, len(pat[k]))
+				copy(new_pat[k], pat[k])
+			}
+
+			new_pat[i][j] = flipsymbol(p.Pattern[i][j])
+			p := Pattern{Pattern: new_pat}
+			vert := p.findvertical()
+
+			for _, ax := range vert {
+				if ax != 0 && ax != originalVert {
+					// fmt.Printf("vertical match at %d, smudge in (%d,%d) \n", ax, i, j)
+					// p.Print()
+					return ax
+				}
+
+			}
+			hort := p.findhorizontal()
+			for _, ax := range hort {
+				if ax != 0 && ax != originalHort {
+					// fmt.Printf("horizontal match at %d, smudge in (%d,%d) \n", ax, i, j)
+					// p.Print()
+					return ax * 100
+				}
 			}
 		}
 	}
 	return 0
 }
-func (p Pattern) findhorizontal() int {
-	p = p.transpose()
-	return p.findvertical()
-	// fmt.Println(ans)
 
+func flipsymbol(r string) string {
+
+	switch r {
+	case "#":
+		return "."
+	case ".":
+		return "#"
+	default:
+		panic("wrong string detected")
+	}
 }
 
 func min(a, b int) int {
@@ -145,19 +196,30 @@ func partA(input []string) {
 	for i, pattern := range c.Patterns {
 		vert := pattern.findvertical()
 		hort := pattern.findhorizontal()
-		if vert == 0 && hort == 0 {
-			fmt.Println("no axis found for pattern!!!!!", i)
+		if vert[0] == 0 && hort[0] == 0 {
+			fmt.Println("no axis found for pattern", i)
 			break
 		}
-		fmt.Printf("for pattern %d found %d horizontal, %d vertical \n", i, hort, vert)
-		vertical += vert
-		horizontal += hort * 100
+		// fmt.Printf("for pattern %d found %d horizontal, %d vertical \n", i, hort, vert)
+		vertical += vert[0]
+		horizontal += hort[0] * 100
 	}
-	fmt.Println("ans:", vertical+horizontal)
+	fmt.Println("ans part a:", vertical+horizontal)
 
 }
+
 func partB(input []string) {
-	fmt.Println("part_b:")
+	sum := 0
+	c := createCollection(input)
+	for i, pattern := range c.Patterns {
+		// fmt.Printf("pattern %d: \n", i)
+		found := pattern.iterateandtest()
+		if found == 0 {
+			fmt.Println("no axis found for pattern", i)
+		}
+		sum += found
+	}
+	fmt.Println("ans part b", sum)
 }
 
 func main() {
@@ -168,6 +230,6 @@ func main() {
 	splitInput := strings.Split(strings.TrimSpace(load_file), "\n")
 
 	partA(splitInput)
-	// partB(splitInput)
+	partB(splitInput)
 
 }
