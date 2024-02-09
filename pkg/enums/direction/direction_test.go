38fb401433fa549cc9add4cd9f881afebb31a5d3
Martti Yap <martti.yap@gmail.com>
2024-02-09 11:54:53 +0100

    increase test coverage

--- pkg/enums/direction/direction_test.go
+++ pkg/enums/direction/direction_test.go
@@ -1,6 +1,7 @@
 package direction
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -19,6 +20,32 @@ func TestOpposite(t *testing.T) {
 		result := test.input.Opposite()
 		if result != test.expected {
 			t.Errorf("Opposite(%s) = %s; want %s", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	// Test cases for Opposite method
+	tests := []struct {
+		input    Direction
+		addend   int
+		expected Direction
+	}{
+		{North, 1, East},
+		{North, 2, South},
+		{North, 3, West},
+		{North, 4, North},
+		{West, 2, East},
+		{East, 4, East},
+		{East, 40, East},
+		{East, 4000, East},
+		{East, -4, East},
+		{East, -4000, East},
+	}
+	for _, test := range tests {
+		result := test.input.Add(test.addend)
+		if result != test.expected {
+			t.Errorf("%s + %s = %s; want %s", test.input, strconv.Itoa(test.addend), result, test.expected)
 		}
 	}
 }
