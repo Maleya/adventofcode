8f8ed4a4ab83d05ebb3d0960b6adb21a6904d4b4
Martti Yap <martti.yap@gmail.com>
2024-02-15 11:28:35 +0100

    part 2 preoptimal

--- src/2023/day17/solution.go
+++ src/2023/day17/solution.go
@@ -81,6 +81,115 @@ func (c *crucible) FindLegalDirections(g Grid) []direction.Direction {
 
 }
 
+type ultracrucible struct {
+	y, x         int
+	heatloss     int
+	lastTenMoves []direction.Direction
+	allmoves     []direction.Direction
+}
+
+func (c ultracrucible) String() string {
+	return fmt.Sprintf("location (%v, %v) heatloss %v, history %v", c.y, c.x, c.heatloss, c.lastTenMoves)
+}
+
+func (c ultracrucible) lastNmovesEqualto(n int, dir direction.Direction) bool {
+
+	noElements := len(c.lastTenMoves)
+	// lowerbound := noElements - n
+	// if noElements-n < 0 {
+	// 	lowerbound = 0
+	// }
+
+	for i := noElements - 1; i >= noElements-n; i-- {
+		if c.lastTenMoves[i] != dir {
+			return false
+		}
+	}
+	return true
+}
+
+func (c ultracrucible) lastNmovesEqual(n int) bool {
+
+	noElements := len(c.lastTenMoves)
+	dir := c.lastTenMoves[noElements-1]
+
+	for i := noElements - 1; i >= noElements-n; i-- {
+		if c.lastTenMoves[i] != dir {
+			return false
+		}
+	}
+	return true
+}
+
+func (c *ultracrucible) SpawnNewCrucibleInDirection(dir direction.Direction, g Grid) ultracrucible {
+	var newDirection []direction.Direction
+	var allmoves []direction.Direction
+
+	y, x := g.displaceByDirection(c.y, c.x, dir)
+	if !g.isonGrid(y, x) {
+		panic("not on grid")
+	}
+
+	newDirection = append(newDirection, c.lastTenMoves...)
+	newDirection = append(newDirection, dir)
+
+	allmoves = append(allmoves, c.allmoves...)
+	allmoves = append(allmoves, dir)
+	if len(newDirection) > 10 {
+		newDirection = newDirection[1:]
+	}
+
+	return ultracrucible{
+		y:            y,
+		x:            x,
+		heatloss:     c.heatloss + g.grid[y][x],
+		lastTenMoves: newDirection,
+		allmoves:     allmoves,
+	}
+}
+
+func (cru *ultracrucible) FindLegalDirections(g Grid) []direction.Direction {
+	var legalDirections []direction.Direction
+	options := []direction.Direction{direction.North, direction.East, direction.South, direction.West}
+
+	for _, dir := range options {
+		// dont move off grid
+		if !g.directionOnGrid(cru.y, cru.x, dir) {
+			continue
+		}
+
+		// maximum 10 blocks in one dir
+		if len(cru.lastTenMoves) == 10 {
+			if cru.lastNmovesEqualto(10, dir) {
+				continue
+			}
+		}
+		// minimum 4 blocks forward in same direction
+		moveshistory := len(cru.lastTenMoves)
+		if moveshistory > 4 {
+			moveshistory = 4
+		}
+		if len(cru.lastTenMoves) > 0 {
+			if !cru.lastNmovesEqual(moveshistory) && dir != cru.lastTenMoves[len(cru.lastTenMoves)-1] {
+				continue
+			}
+		}
+
+		// dont move backwards
+		if len(cru.lastTenMoves) > 0 {
+			lastmove := cru.lastTenMoves[len(cru.lastTenMoves)-1]
+			if lastmove.Opposite() == dir {
+				continue
+			}
+		}
+
+		legalDirections = append(legalDirections, dir)
+	}
+	// fmt.Println(legalDirections)
+	return legalDirections
+
+}
+
 type Grid struct {
 	grid          [][]int
 	visitedStates map[string]bool
@@ -158,7 +267,7 @@ func partA(input []string) {
 		if cru.heatloss > leastHeatloss {
 			continue
 		}
-		// implement manhattan distance check
+
 		if grid.inGoal(cru.y, cru.x) {
 			if cru.heatloss < leastHeatloss {
 				fmt.Println("GOAL", cru)
@@ -190,7 +299,55 @@ func partA(input []string) {
 
 }
 func partB(input []string) {
-	fmt.Println("part_b:")
+	grid := parseInput(input)
+	fmt.Println(grid)
+	leastHeatloss := math.MaxInt64
+	cache := make(map[string]int)
+
+	cru := ultracrucible{y: 0, x: 0, heatloss: 0, lastTenMoves: []direction.Direction{}}
+	pq := []ultracrucible{cru}
+
+	for len(pq) > 0 {
+		cru := pq[0]
+		pq = pq[1:]
+		// fmt.Println(cru)
+
+		if cru.heatloss > leastHeatloss {
+			continue
+		}
+		// must end on 4x of the same direction
+		if grid.inGoal(cru.y, cru.x) && cru.lastNmovesEqual(4) {
+			if cru.heatloss < leastHeatloss {
+				fmt.Println("GOAL", cru)
+				fmt.Println(cru.allmoves)
+
+				leastHeatloss = cru.heatloss
+			}
+			continue
+		}
+
+		// enc := fmt.Sprintf("(%v, %v),%v,%v,%v", cru.y, cru.x, latestMove, lastFourbool, lastTenBool)
+		enc := fmt.Sprintf("(%v, %v),%v,", cru.y, cru.x, cru.lastTenMoves)
+		// fmt.Println("enc", enc)
+		if _, ok := cache[enc]; ok {
+			continue
+		} else {
+			cache[enc] = cru.heatloss
+		}
+
+		// make sure we want to spawn more?
+		newDirecitons := cru.FindLegalDirections(grid)
+		if len(newDirecitons) > 0 {
+			for _, dir := range newDirecitons {
+				NewCrucible := cru.SpawnNewCrucibleInDirection(dir, grid)
+				pq = append(pq, NewCrucible)
+			}
+		}
+		sort.SliceStable(pq, func(i, j int) bool {
+			return pq[i].heatloss < pq[j].heatloss
+		})
+	}
+
 }
 
 func main() {
@@ -198,7 +355,6 @@ func main() {
 	// load_file := input
 	splitInput := strings.Split(strings.TrimSpace(string(load_file)), "\n")
 
-	partA(splitInput)
+	// partA(splitInput)
 	partB(splitInput)
-
 }
